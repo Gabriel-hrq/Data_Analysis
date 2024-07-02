6ed1c04f43f8bfbc6ddddd49216cae67975558a7
Gabriel-hrq <gabriel.hrq@ufu.br>
2024-07-02 18:57:50 -0300

    Análise de faturamento: Criação de funçoes para vizualizar os dados, relacionar graficamente Faturamento x Região, e descrição de variáveis

--- data_analytics.py
+++ data_analytics.py
@@ -20,3 +20,36 @@ pd.set_option('display.max_columns', None)
 
 
 df = pd.read_excel(r"C:/Users/User/Downloads/PEDIDOS_INDUSTRIA_TEXTIL_V1.xlsx", sheet_name="BASE")
+
+uf = df.groupby(by="REGIAO")["VLRFATURADO"].sum().reset_index()
+#Output sobre as variáveis
+def variables_description(df):
+    print('Variáveis da base de dados e seus tipos: \n')
+    print(df.info(),"\n")
+    
+
+def analise_faturamento(uf):
+    uf = uf.rename(columns={"VLRFATURADO": "VLRFATURADO_REGIAO"})
+    print(uf,"\n")
+
+
+
+def bar_graph_plot(x,y,title,uf):
+    plt.rcParams["figure.figsize"] = (20,5)
+    ax = uf.plot(kind='bar')
+    ax.set_xticklabels(uf[x], minor=False)
+    plt.title(title)
+    plt.ylabel(y)
+    plt.xlabel(x)
+    for p in ax.patches:
+        ax.annotate(str(p.get_height()), (p.get_x(), p.get_height() * 1.10))
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    variables_description(df)
+    analise_faturamento(uf)
+    bar_graph_plot("REGIAO","VLRFATURADO","Valor faturado por região", uf)  #Grafico faturamento x regiao
+
+main()
